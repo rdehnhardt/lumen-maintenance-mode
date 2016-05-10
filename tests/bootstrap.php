f2f2b2e36bcd96d014d35ec56877024d97dc3ebe
Renato Dehnhardt <renato.dehnhardt@rentcars.com>
2016-05-10 12:56:10 -0300

    FIX CS-FIXER

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,8 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__.'/../vendor/autoload.php';
 
-$lumenAppDir = __DIR__ . '/../vendor/laravel/lumen';
+$lumenAppDir = __DIR__.'/../vendor/laravel/lumen';
 
 /*
 |--------------------------------------------------------------------------
